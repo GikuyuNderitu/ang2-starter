d97d43126c126530473250576a239e9ee90f2070
Gikuyu <gikuyunderitu@gmail.com>
2017-06-22 04:04:33 -0400

    Set up index.html redirect and prefix routes with '/api/*' to account for proxies and future public apis

--- server/utils/routes.js
+++ server/utils/routes.js
@@ -2,10 +2,10 @@ const users = require('./../controllers/users.js');
 module.exports = function (app) {
 	//Set up routes
 	// Index route
-	app.get('/', users.index);
+	app.get('/api/users', users.index);
 
-	app.all("*", (req, res) => {
-		res.redirect("/");
+	app.all("*", (req, res, next) => {
+		res.sendfile(path.resolve("./public/dist/index.html"));
 	})
 
 };
