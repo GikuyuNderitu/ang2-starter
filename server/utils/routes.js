fbbc8a9105426f17699fa1fd60851929520bb409
Gikuyu Nderitu <gikyunderitu@gmail.com>
2017-08-22 11:15:58 -0400

    Update route file to include resolve

--- server/utils/routes.js
+++ server/utils/routes.js
@@ -1,11 +1,12 @@
 const users = require('./../controllers/users.js');
+const {resolve} = require('path');
 module.exports = function (app) {
 	//Set up routes
 	// Index route
 	app.get('/api/users', users.index);
 
 	app.all("*", (req, res, next) => {
-		res.sendfile(path.resolve("./public/dist/index.html"));
+		res.sendfile(resolve("./public/dist/index.html"));
 	})
 
 };
